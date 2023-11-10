1a8dda775be722bb9610199f801e2af74e6b643f
theerapat-s28 <theerapat.s28@gmail.com>
2023-11-11 01:22:07 +0700

    [main] update gitignore

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import subprocess, os
-import settings
+import settings, hostname_list
 import logger
+
+import https
 
 
 def main():
-  log_file = os.path.join(settings.LOG_FOLDER_PATH, "cron.log")
+  # log_file = os.path.join(settings.LOG_FOLDER_PATH, "cron.log")
   
-  if not os.path.exists(log_file):
-    logger.create_new_file(log_file)
+  # if not os.path.exists(log_file):
+  #   logger.create_new_file(log_file)
 
-  logger.write(log_file, 'Hello Cron')
+  # logger.write(log_file, 'Hello Cron')
+  print(https.ssl_days_left(hostname_list.HOSTNAMES[0]['name']))
 
 
 if __name__ == "__main__":
--- settings.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-LOG_FOLDER_PATH = '/home/khunbai/01-dev_projects/05-python_subprocess/'
